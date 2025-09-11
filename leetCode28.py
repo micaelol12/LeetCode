3d223b68ba53918f53f884957f5633df861f1a20
Micael Luan Conti <micael.conti@govbr.com.br>
2025-09-11 15:05:43 -0300

    leetcode 28

--- leetCode28.py
+++ leetCode28.py
@@ -1,21 +1,19 @@
-from typing import List
-
-
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        x = 0
-
-        for i,c in enumerate(haystack):
-            if x == len(needle):
-                return i - x
+        needle_l = len(needle) 
+        
+        for i in range(len(haystack) - needle_l + 1):
+            x  = 0
             
-            if c == needle[x]:
-                x +=1
-            else:
-                x = 0
-
+            while x < needle_l and haystack[i + x] == needle[x]:
+                x += 1
+            
+            if x == needle_l:
+                return i
 
         return -1
-
+    
 d = Solution()
-print(d.strStr("butsad","sad"))
+print(d.strStr("mississippi","issipi"))
+print(d.strStr("aa","aaa"))
+print(d.strStr("sadbutsad","sad"))
